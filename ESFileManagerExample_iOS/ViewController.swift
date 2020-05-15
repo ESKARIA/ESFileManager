5a3b4fdb41d7d40051c3275c132a9125ce571241
Dmitriy <ToropkinD@gmail.com>
2020-05-15 16:02:57 +0300

    [in-progress] custom path maybe work

--- ESFileManagerExample_iOS/ViewController.swift
+++ ESFileManagerExample_iOS/ViewController.swift
@@ -54,7 +54,7 @@ final class ViewController: UIViewController {
         }
     }
     
-    /// Write file on default directory
+    /// Write file on custom directory
     private func writeFileCustomDirectory() {
         // create data to write
         let data = "Swift is amazing!".data(using: .utf8)
@@ -63,7 +63,7 @@ final class ViewController: UIViewController {
         let file = ESFileModel(data: data, name: fileName)
         
         // write data. At = nil cause we use default directory from prepareVC method
-        fileManager.write(file: file, at: .documents(urlPath: "Test", useBackups: false)) { (error) in
+        fileManager.write(file: file, at: .documents(customPath: "Test", useBackups: false)) { (error) in
             if let error = error {
                 self.logString = "You don't record the file with error: \(error.localizedDescription)"
                 return
@@ -79,6 +79,29 @@ final class ViewController: UIViewController {
         
         //read this file. At = nil cause we use default directory
         fileManager.read(fileStorage: file, at: nil) { (file, error) in
+            //fetch error
+            if let error = error {
+                self.logString = "You don't read the file with error: \(error.localizedDescription)"
+                return
+            }
+            //fetch empty state of file
+            guard let data = file?.data else {
+                self.logString = "You have read file but its empty!"
+                return
+            }
+            //show our file
+            let stringFile = String(data: data, encoding: .utf8)
+            self.logString = "You successful read the file! File is: \n \(String(describing: stringFile))"
+        }
+    }
+    
+    /// Read file from custom  directory
+    private func readFileFromCustomDirectory() {
+        // create name and extension of file to read
+        let file = ESFileNameModel(name: "SwiftDoc", fileExtension: .txt)
+        
+        //read this file. At = nil cause we use default directory
+        fileManager.read(fileStorage: file, at: .documents(customPath: "Test", useBackups: false)) { (file, error) in
             //fetch error
             if let error = error {
                 self.logString = "You don't read the file with error: \(error.localizedDescription)"
@@ -115,7 +138,7 @@ final class ViewController: UIViewController {
     
         switch sender.tag {
         case 0: writeFile()
-        default: logString = "Custom directory doesnt support right now!"
+        default: writeFileCustomDirectory()
         }
     }
     
@@ -124,7 +147,7 @@ final class ViewController: UIViewController {
         
         switch sender.tag {
         case 0: readFile()
-        default: logString = "Custom directory doesnt support right now!"
+        default: readFileFromCustomDirectory()
         }
     }
     
